5eb87b613808a82362e03c4eebeb625ba941254d
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-06-30 11:20:52 -0300

    criando projeto

---
+++ .history/data-fetching/src/App_20220630111248.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+export default function App() {
+ return (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+ )
+}
+
+
